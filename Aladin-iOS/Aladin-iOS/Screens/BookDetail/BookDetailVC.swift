e96fd128c501da2a05c808f18b49b41a57e9ed9c
6uohul <kiy010604@naver.com>
2022-11-16 17:59:21 +0900

    [Chore] #5 스크롤 뷰 레이아웃 추가

--- Aladin-iOS/Aladin-iOS/Screens/BookDetail/BookDetailVC.swift
+++ Aladin-iOS/Aladin-iOS/Screens/BookDetail/BookDetailVC.swift
@@ -12,17 +12,32 @@ class BookDetailVC: UIViewController {
     //MARK: - UI Components
     
     // 스크롤 뷰
-    private let contentScrollView: UIScrollView = {
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.backgroundColor = .white
-        scrollView.showsVerticalScrollIndicator = false
-        
-        return scrollView
-    }()
+    private let contentScrollView = UIScrollView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .white
+        $0.showsVerticalScrollIndicator = true
+    }
+    
+    private let contentView = UIView().then{
+        $0.backgroundColor = .white
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setLayout()
     }
     
+}
+
+extension BookDetailVC {
+    func setLayout() {
+        view.addSubviews(contentScrollView, naviView)
+        
+        contentScrollView.addSubviews(contentView)
+        contentScrollView.snp.makeConstraints {
+            $0.top.equalTo(naviView.snp.bottom)
+            $0.leading.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+    }
 }
