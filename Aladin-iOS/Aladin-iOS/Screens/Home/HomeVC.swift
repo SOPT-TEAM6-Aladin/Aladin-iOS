ea0e1398f4aaa56f3ea0d3ff4fe8ce24602ad8db
Jonghwa Won <jongwha95@gmail.com>
2022-11-17 01:20:30 +0900

    [Feat] #6-컨테이너 뷰 구현

--- Aladin-iOS/Aladin-iOS/Screens/Home/HomeVC.swift
+++ Aladin-iOS/Aladin-iOS/Screens/Home/HomeVC.swift
@@ -6,12 +6,88 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+
 
 final class HomeVC: UITabBarController {
     
+    private let searchContainerView = UIView()
+    private let bannerImageView = UIImageView()
+    private let categoryContainerView = UIView()
+    private let editerChoiceContainerView = UIView()
+    private let hotBookContainerView = UIView()
+    private let giftContainerView = UIView()
+    
+    private let menuButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.Icons.menu.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+    private let barcodeButton = UIButton(type: .system).then {
+        $0.setImage(ImageLiterals.Icons.barcode.withRenderingMode(.alwaysOriginal), for: .normal)
+    }
+    
+
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubviews(searchContainerView,bannerImageView,categoryContainerView,editerChoiceContainerView,hotBookContainerView,giftContainerView)
+        
+        searchContainerView.addSubviews(menuButton,barcodeButton)
+        searchContainerView.backgroundColor = .yellow
+        categoryContainerView.backgroundColor = .aladinBlue
+        hotBookContainerView.backgroundColor = .aladinPink2
+        giftContainerView.backgroundColor = .red
+        
+        searchContainerView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(52)
+        }
+        
+        menuButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(20)
+            make.centerY.equalToSuperview()
+        }
+        
+        barcodeButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().inset(20)
+            make.centerY.equalToSuperview()
+        }
+        
+        bannerImageView.snp.makeConstraints { make in
+            make.top.equalTo(searchContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(119)
+        }
+        
+        categoryContainerView.snp.makeConstraints { make in
+            make.top.equalTo(bannerImageView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(209)
+        }
+        
+        editerChoiceContainerView.snp.makeConstraints { make in
+            make.top.equalTo(categoryContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(330)
+        }
+        
+        hotBookContainerView.snp.makeConstraints { make in
+            make.top.equalTo(editerChoiceContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(343)
+        }
+        
+        giftContainerView.snp.makeConstraints { make in
+            make.top.equalTo(hotBookContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(279)
+        }
+        
+        
+    
     }
+    
+    
 }
