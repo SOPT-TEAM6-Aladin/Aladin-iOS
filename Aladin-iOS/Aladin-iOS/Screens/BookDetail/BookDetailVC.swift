e8ceab26303ad4df02b95fb0b0309726629c79d8
6uohul <kiy010604@naver.com>
2022-11-16 14:44:50 +0900

    [Add] #5 책 상세보기 뷰 생성

---
+++ Aladin-iOS/Aladin-iOS/Screens/BookDetail/BookDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  BookDetailVC.swift
+//  Aladin-iOS
+//
+//  Created by 김인영 on 2022/11/16.
+//
+
+import UIKit
+
+class BookDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
