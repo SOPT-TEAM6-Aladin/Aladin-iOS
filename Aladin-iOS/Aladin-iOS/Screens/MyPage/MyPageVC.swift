dea5ee00fb011c97156cbb90066b0ec9bb5e4989
Jonghwa Won <jongwha95@gmail.com>
2022-11-24 04:44:31 +0900

    [Feat] #14 - 전체 스크롤뷰 구현, aladinBlue2 색상 추가

--- Aladin-iOS/Aladin-iOS/Screens/MyPage/MyPageVC.swift
+++ Aladin-iOS/Aladin-iOS/Screens/MyPage/MyPageVC.swift
@@ -6,12 +6,49 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 final class MyPageVC: UITabBarController {
+    
+    private let scrollView = UIScrollView()
+    
+    private let myInfoContainerView = UIView()
+    private let tradeListContainerView = UIView()
+    private let footerContainerView = UIView()
+    
     
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(scrollView)
+        myInfoContainerView.backgroundColor = .aladinBlue2
+        footerContainerView.backgroundColor = .red
+        
+        scrollView.addSubviews(myInfoContainerView,tradeListContainerView,footerContainerView)
+        
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        myInfoContainerView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(276)
+        }
+        
+        tradeListContainerView.snp.makeConstraints { make in
+            make.top.equalTo(myInfoContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(541)
+        }
+        
+        footerContainerView.snp.makeConstraints { make in
+            make.top.equalTo(tradeListContainerView.snp.bottom)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(283)
+            make.bottom.equalToSuperview()
+        }
     }
 }
