f97e662da5c44621febe72b659f51f27213cfa3d
6uohul <kiy010604@naver.com>
2022-11-25 17:27:44 +0900

    [Feat] #12 장바구니 푸터 뷰 추가

--- Aladin-iOS/Aladin-iOS/Screens/Basket/BasketFooterView.swift
+++ Aladin-iOS/Aladin-iOS/Screens/Basket/BasketFooterView.swift
@@ -6,15 +6,90 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class BasketFooterView: UITableViewHeaderFooterView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    static let identifier = "BasketFooterView"
+    
+    private let divider1 = UIView().then {
+        $0.backgroundColor = .aladinGray1
     }
-    */
-
+    
+    private let priceLabel = UILabel().then {
+        $0.text = "상품금액"
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .aladinGray6
+    }
+    
+    private let price = UILabel().then {
+        $0.text = "10,800원"
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .black
+    }
+    
+    private let deliveryPriceLabel = UILabel().then {
+        $0.text = "배송비"
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .aladinGray6
+    }
+    
+    private let deliveryPrice = UILabel().then {
+        $0.text = "0원"
+        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.textColor = .black
+    }
+    
+    private let divider2 = UIView().then {
+        $0.backgroundColor = .aladinGray1
+    }
+    
+    //MARK: - Life Cycle
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        setLayout()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setLayout() {
+        addSubviews(
+            divider1,
+            priceLabel,
+            price,
+            deliveryPriceLabel,
+            deliveryPrice,
+            divider2
+        )
+        
+        divider1.snp.makeConstraints {
+            $0.leading.trailing.top.equalToSuperview()
+            $0.height.equalTo(4)
+        }
+        priceLabel.snp.makeConstraints {
+            $0.top.equalTo(divider1.snp.bottom).offset(16)
+            $0.leading.equalToSuperview().offset(20)
+        }
+        price.snp.makeConstraints {
+            $0.centerY.equalTo(priceLabel)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+        deliveryPriceLabel.snp.makeConstraints {
+            $0.top.equalTo(price.snp.bottom).offset(16)
+            $0.leading.equalTo(priceLabel)
+        }
+        deliveryPrice.snp.makeConstraints {
+            $0.centerY.equalTo(deliveryPriceLabel)
+            $0.trailing.equalTo(price)
+        }
+        divider2.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(4)
+        }
+    }
 }
